3def0bbe1c11db75bc4297bcc7eec55f180f5e30
sogbdn <sonia.barretdenazaris@gmail.com>
2018-12-09 17:41:06 +0000

    Initial commit with all my files js

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (nb1, nb2, nb3) {
+  return nb2 * nb3 + nb1;
+}
+console.log(math(53, 61, 67));
---
+++ functions.js
@@ -0,0 +1,5 @@
+function eat (food) {
+  return food + ' tasted really good.';
+};
+var food = 'bananas';
+console.log(eat(food));
